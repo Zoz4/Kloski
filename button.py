91bda3ab4848c2f2c97e79985b4ebaa92a5e505f
Tomspiano <helloworldbrandnew@163.com>
2020-10-18 19:02:47 +0800

    formatting

--- button.py
+++ button.py
@@ -1,9 +1,9 @@
 import pygame.font
 
-class Button():
 
-    def __init__(self, ai_settings, screen, msg):
-        '''初始化按钮的属性'''
+class Button:
+    def __init__(self, screen, msg):
+        """初始化按钮的属性"""
         self.screen = screen
         self.screen_rect = screen.get_rect()
 
@@ -18,17 +18,12 @@ class Button():
         self.rect.center = self.screen_rect.center
 
         # 按钮的标签只需要创建一次
-
-        self.prep_msg(msg)
-
-    def prep_msg(self, msg):
-        '''将msg渲染为图像，并使其在按钮上居中'''
+        """将msg渲染为图像，并使其在按钮上居中"""
         self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
 
     def draw_button(self):
-        '''绘制一个用颜色填充的按钮，再绘制文本'''
+        """绘制一个用颜色填充的按钮，再绘制文本"""
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
-
--- game_stats.py
+++ game_stats.py
@@ -1,10 +1,12 @@
-class GameStats():
-    '''跟踪游戏的统计信息'''
+class GameStats:
+    """跟踪游戏的统计信息"""
+
     def __init__(self, ai_settings):
-        '''初始化统计信息'''
+        """初始化统计信息"""
         self.ai_settings = ai_settings
         self.reset_stats()
         self.game_active = False
+
     def reset_stats(self):
-        '''初始化在游戏运行期间可能变化的统计信息'''
+        """初始化在游戏运行期间可能变化的统计信息"""
         return

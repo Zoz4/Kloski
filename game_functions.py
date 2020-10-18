0676429a5ccf6e26eb4465d4414d38b248acdd9e
Tomspiano <helloworldbrandnew@163.com>
2020-10-18 19:02:25 +0800

    add shortcut for quiting game

--- game_functions.py
+++ game_functions.py
@@ -3,7 +3,7 @@ import pygame
 
 
 def check_keydown_event(event, blocks):
-    '''响应按键'''
+    """响应按键"""
     if event.key == pygame.K_w:
         blocks.move('w')
     elif event.key == pygame.K_s:
@@ -13,30 +13,37 @@ def check_keydown_event(event, blocks):
     elif event.key == pygame.K_d:
         blocks.move('d')
 
+
 def check_events(stats, blocks, play_button):
-    '''响应按键和鼠标事件'''
+    """响应按键和鼠标事件"""
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or \
+                event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+            pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             check_keydown_event(event, blocks)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             check_play_button(stats, blocks, play_button, mouse_x, mouse_y)
+
+
 def check_win(blocks):
-    '''判断是否恢复'''
-    return  blocks.status == blocks.goal
+    """判断是否恢复"""
+    return blocks.status == blocks.goal
+
 
 def check_play_button(stats, blocks, play_button, mouse_x, mouse_y):
-    '''在玩家单击Play按钮时开始新游戏'''
+    """在玩家单击Play按钮时开始新游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
 
     if button_clicked and not stats.game_active:
         blocks.break_order()
         stats.game_active = True
 
+
 def update_screen(ai_settings, screen, stats, blocks, play_button):
-    '''更新屏幕上的图像，并切换到新图像'''
+    """更新屏幕上的图像，并切换到新图像"""
     # 设置背景颜色
     screen.fill(ai_settings.bg_color)
     # 绘制数字方块
@@ -46,4 +53,3 @@ def update_screen(ai_settings, screen, stats, blocks, play_button):
         play_button.draw_button()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
-
